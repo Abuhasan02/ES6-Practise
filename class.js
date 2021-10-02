3b716765f7403f0dd59c17758bbf860c961f5603
Abu Hasan <hasanabubachelor@gmail.com>
2021-10-02 15:52:23 +0600

    class

---
+++ class.js
@@ -0,0 +1,13 @@
+class Car{
+    constructor(name,color,year){
+        this.name = name;
+        this.color = color;
+        this.year = year;
+    }
+
+}
+const car1 = new Car("Ford","Black",2014);
+const car2 = new Car("Toyota","Red",2015);
+
+console.log(car1.name);
+console.log(car2);
